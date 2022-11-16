9a29d2f8cd066580be20f8122035cca835ac73e4
Victor Micheni <victormichenim@gmail.com>
2022-11-17 00:51:21 +0300

    Added background and created players

--- main.py
+++ main.py
@@ -1,25 +1,63 @@
 import pygame
 
+from fighter import Fighter
+
 pygame.init()
 
 # Create game window
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 
+
 screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Brawler")
 
+# set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+
 # Load background image
-bg_image = pygame.image.load("")
+bg_image = pygame.image.load(
+    "Assets/images/background/background.jpg").convert_alpha()
+
+# Function for drawing background image
+
+
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0, 0))
+
+
+# Create two instances of fighters
+scorpion = Fighter(200, 310)
+subzero = Fighter(700, 310)
 
 # Game loop
 run = True
 while run:
 
+    clock.tick(FPS)
+
+    # draw background image
+    draw_bg()
+
+    # move fighters
+    scorpion.move()
+    subzero.move()
+
+    # Draw fighter
+    scorpion.draw(screen)
+    subzero.draw(screen)
+
     # Event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    # update display
+    pygame.display.update()
+
 
 # exit pygame
 pygame.quit()
